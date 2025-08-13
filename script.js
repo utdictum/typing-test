699a46cd3ff264f76811e4a45f5ec40a870a9f71
utdictum <43791685+utdictum@users.noreply.github.com>
2025-08-13 13:43:52 +0300

    feat: main elements and variables

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("loading script")
+
+const targetEl = document.getElementById("target-text");
+const typedEl = document.getElementById("typed-text");
+const wpmEl = document.getElementById("wpm-display");
+const accEl = document.getElementById("accuracy-display");
+const instructions = document.getElementById("instructions");
+
+let targetText = "Start typing to begin the test.";
+let typed = "";
