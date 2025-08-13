558db2dc4aa6f420b904fda0e4265e82a44b920f
utdictum <43791685+utdictum@users.noreply.github.com>
2025-08-13 14:02:31 +0300

    feat: add wpm & acc calc % stats update

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ function render() {
   typedEl.textContent = typed;
 }
 render();
+
+let startTime = null;
+
+/*  calcs wpm
+    elapsed time in miliseconds since typing began is converted m
+    assumes word length is 5 and devides
+*/
+
+function calcWPM() {
+  if (!startTime) return 0;
+  const minutes = (Date.now() - startTime) / 60000;
+  const words = typed.length / 5;
+  return Math.max(0, Math.round(words / Math.max(minutes, 1e-6))); //avoids division by 0 and rounds to integer
+}
+/*  compares typed txt to target txt and calcs accuracy %
+    if nothing typed = 100
+    loops caracters to find matches
+*/
+function calcAccuracy() {
+  if (typed.length === 0) return 100;
+  let correct = 0;
+  for (let i = 0; i < typed.length; i++) {
+    if (typed[i] === targetText[i]) correct++;
+  }
+  return Math.round((correct / typed.length) * 100);
+}
+
+function updateStats() {
+  wpmEl.textContent = "WPM: " + calcWPM();
+  accEl.textContent = "Accuracy: " + calcAccuracy() + "%";
+}
+
+updateStats();
